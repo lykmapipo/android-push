ce7c8ab9eb2b134a4ef9b64523018138007b685f
lykmapipo <lallyelias87@gmail.com>
2016-10-16 16:27:59 +0300

    add sample project

---
+++ app/src/main/java/com/github/byteskode/push/MainActivity.java
@@ -0,0 +1,34 @@
+package com.github.byteskode.push;
+
+import android.app.Activity;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import com.github.byteskode.push.R;
+
+
+public class MainActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
