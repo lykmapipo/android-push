53b2e582e3ae1e5b5f476ecb9a5cc387162a21bf
lykmapipo <lallyelias87@gmail.com>
2016-10-16 19:27:35 +0300

    fix sample package collision with a library

--- app/src/main/java/com/github/byteskode/push/sample/MainActivity.java
+++ app/src/main/java/com/github/byteskode/push/sample/MainActivity.java
@@ -1,20 +1,11 @@
-package com.github.byteskode.push;
+package com.github.byteskode.push.sample;
 
 import android.app.Activity;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Context;
-import android.content.Intent;
-import android.media.RingtoneManager;
-import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.NotificationCompat;
-import android.util.Log;
-
 import com.github.byteskode.push.R;
 
 
-public class MainActivity extends Activity{
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- library/src/main/java/com/github/byteskode/push/Push.java
+++ library/src/main/java/com/github/byteskode/push/Push.java
@@ -4,45 +4,60 @@ package com.github.byteskode.push;
 import android.content.Context;
 
 
-/** 
+/**
  * Singleton to manage push notifitication
- * 
+ *
  * @author lally elias(lykmapipo), byteskode Team & Contibutors
  * @email lallyelias87@gmail.com, lally.elias@byteskode.com
  */
 public class Push {
-	/**
-	 * class instance
-	 */
-	private static Push instance = null;
+    /**
+     * key used to stoke latest fcm registration token
+     */
+    public static final String REGISTRATION_ID_PREF_KEY = "registrationToken";
 
-	/**
-	 * holding context
-	 */
-	private Context mContext;
+    /**
+     * key used to store application instance id
+     */
+    public static final String INSTANCE_ID_PREF_KEY = "instanceId";
 
-	/**
+    /**
+     * class instance
+     */
+    private static Push instance = null;
+
+    /**
+     * holding context
+     */
+    private Context mContext;
+
+    /**
      * Private constructor
      *
      * @param mContext
      */
-	private Push(Context mContext){
-		this.mContext = mContext;
-		init();
-	}
+    private Push(Context mContext) {
+        this.mContext = mContext;
+//		init();
+    }
 
 
-	/**
+    /**
      * Singleton instance method
      *
      * @param context
      * @return
      */
     public static Push getInstance(Context context) {
-        if (instance == null){
-        	instance = new Push(context);
+        if (instance == null) {
+            instance = new Push(context);
         }
         return instance;
+    }
+
+    public void getInstanceId() {
+//        new GoogleApiClient();
+//        String iid = InstanceID.getInstance(this.mContext).getId();
     }
 
 }
